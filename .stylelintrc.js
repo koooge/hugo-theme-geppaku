e06c498de54da6723b53f2f2eb752c0c67fecc35
koooge <koooooge@gmail.com>
2022-02-02 02:04:09 +0100

    fix: Fix stylelint
    Signed-off-by: koooge <koooooge@gmail.com>

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: ['stylelint-config-standard'],
   rules: {
     // TODO: Fix these rules
-    'declaration-block-no-shorthand-property-overrides': null,
     'no-descending-specificity': null,
   },
 }
